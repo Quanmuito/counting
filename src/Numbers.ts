16f16d75fe30692fd70ac12e235cfa169ee8158c
quanmuito <tranleminhquan1102@gmail.com>
2021-08-09 20:49:14 +0300

    init

---
+++ src/Numbers.ts
@@ -0,0 +1,16 @@
+type NumbersType = {
+    [index: number]: string
+}
+
+export const BaseNumbers: NumbersType = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+    10: 'Ten',
+};
